b6cf59229fb1cdbdfa177d4f89d1919baf15ed7d
gamer13433 <saintpi70@gmail.com>
2021-07-12 12:51:59 +0500

    tree: some improvments for devfreq_boost

--- drivers/video/fbdev/exynos/dpu30_dual/decon_dsi.c
+++ drivers/video/fbdev/exynos/dpu30_dual/decon_dsi.c
@@ -418,6 +418,7 @@ static int decon_vsync_thread(void *data)
 
 int decon_create_vsync_thread(struct decon_device *decon)
 {
+        struct sched_param param = { .sched_priority = 20 };
 	int ret = 0;
 	char name[16];
 
@@ -440,6 +441,8 @@ int decon_create_vsync_thread(struct decon_device *decon)
 		ret = PTR_ERR(decon->vsync.thread);
 		goto err;
 	}
+
+        sched_setscheduler_nocheck(decon->vsync.thread, SCHED_FIFO, &param);
 
 	return 0;
 
